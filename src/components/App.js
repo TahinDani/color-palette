4795c3eaa054a8e5e72b58168023255ac2d8f069
Daniel Tahin <tahin.daniel@gmail.com>
2020-04-14 18:12:46 +0200

    Add and style color slider

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Palette from './Palette'
-import seedColors from '../seedColors'
+import seedColors from '../helpers/seedColors'
+import { generatePalette } from '../helpers/colorRangeHelper'
 
 
 function App() {
-  return (
-	<div>
-		<Palette {...seedColors[4]}/>
-	</div>
-  );
+	return (
+		<div>
+			<Palette palette={generatePalette(seedColors[4])}/>
+		</div>
+	);
 }
 
 export default App;
