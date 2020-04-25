07cb4ab792333199321cecee21de9eed754073c2
Daniel Tahin <tahin.daniel@gmail.com>
2020-04-25 10:48:56 +0200

    Add format change, footer and routes

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -5,30 +5,40 @@ import '../styles/Palette.css'
 
 class Palette extends Component {
 	constructor(props) {
-		super(props);
-		this.state = { level: 500 };
-		this.changeLevel = this.changeLevel.bind(this);
+		super(props)
+		this.state = {
+			level: 500,
+			format: "hex"
+		}
 	}
 	
-	changeLevel(level) {
-		this.setState({ level });
+	changeLevel = (level) => {
+		this.setState({ level })
+	}
+
+	changeFormat = (val) => {
+		this.setState({format: val})
 	}
 
 	render() {
-		const { colors } = this.props.palette;
-		const { level } = this.state;
+		const { colors, paletteName, emoji } = this.props.palette;
+		const { level, format } = this.state;
 		let colorBoxes = colors[level].map(color => (
-			<ColorBox background={color.hex} name={color.name} />
+			<ColorBox background={color[format]} name={color.name} key={color.hex}/>
 		))
 		return (
 			<div className="Palette">
-				<Navbar level={level} changeLevel={this.changeLevel} />
+				<Navbar level={level} changeLevel={this.changeLevel} handleFormatChange={this.changeFormat}/>
 				<div className="Palette-colors">
 					{colorBoxes}
 				</div>
+				<footer>
+					{paletteName}
+					<span>{emoji}</span>
+				</footer>
 			</div>
-		);
+		)
 	}
 }
 
-export default Palette;
+export default Palette
